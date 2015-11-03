f77477364b51a3b57437d7dd97171db6cb47a8c3
Andrew Menning <menning23@gmail.com>
2015-11-02 23:05:28 -0500

    Lesson 20 Sweet Dynamic HTML Program

---
+++ Lesson_20_Sweet_Dynamic_HTML_Program/bucky.js
@@ -0,0 +1,23 @@
+function dude(){
+	table = document.getElementById("table");
+	tableHead = document.getElementById("tableHead");
+	item1 = document.getElementById("item1");
+	item2 = document.getElementById("item2");
+	
+	table.className = "dudeTable";
+	tableHead.className = "dudeHead";
+	item1.className = "dudeItem";
+	item2.className = "dudeItem";
+}
+
+function chick(){
+	table = document.getElementById("table");
+	tableHead = document.getElementById("tableHead");
+	item1 = document.getElementById("item1");
+	item2 = document.getElementById("item2");
+	
+	table.className = "chickTable";
+	tableHead.className = "chickHead";
+	item1.className = "chickItem";
+	item2.className = "chickItem";
+}
