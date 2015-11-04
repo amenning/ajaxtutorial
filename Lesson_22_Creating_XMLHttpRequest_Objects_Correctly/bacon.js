dd143402f6f78310270672358fce29ce2f42449a
Andrew Menning <menning23@gmail.com>
2015-11-03 21:54:20 -0500

    Lesson 22 Creating XMLHttpRequest Objects Correctly

---
+++ Lesson_22_Creating_XMLHttpRequest_Objects_Correctly/bacon.js
@@ -0,0 +1,15 @@
+var xmlHttp = createXmlHttpRequestObject();
+
+function createXmlHttpRequestObject(){
+	var xmlHttp;
+	
+	if(window.XMLHttpRequest){
+		//If your browser is aware of this object (90% of the time)
+		xmlHttp = new XMLHttpRequest();
+	}else{
+		//If you are using Microsoft
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	return xmlHttp;
+}
