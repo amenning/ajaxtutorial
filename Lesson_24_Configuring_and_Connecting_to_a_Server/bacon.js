6d2231760f496f934e205709e085235d2ae4a03b
Andrew Menning <menning23@gmail.com>
2015-11-03 22:06:37 -0500

    Lesson 24 Configuring and Connecting to a Server

---
+++ Lesson_24_Configuring_and_Connecting_to_a_Server/bacon.js
@@ -0,0 +1,27 @@
+var xmlHttp = createXmlHttpRequestObject();
+
+function createXmlHttpRequestObject(){
+	var xmlHttp;
+	
+	if(window.XMLHttpRequest){
+		//If your browser is aware of this object (90% of the time)
+		xmlHttp = new XMLHttpRequest();
+	}else{
+		//If you are using Microsoft
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	return xmlHttp;
+}
+
+function process(){
+	if(xmlHttp){
+		try{
+			xmlHttp.open("GET", "bacon.txt", true);
+			xmlHttp.onreadystatechange = handleServerResponse;
+			xmlHttp.send(null);
+		}catch(e){
+			alert(e.toString());
+		}
+	}
+}
